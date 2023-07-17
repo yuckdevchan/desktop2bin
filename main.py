6036ba9f0b85844095b78bdac25f335a9acef205
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-07-17 21:41:09 +0100

    add stuff

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # desktop2bin
 # licensed under the GPL v2 only
 import subprocess
+
+subprocess.run("./xdg_test.sh", shell=True)
+
+if xdg_desktop_exists:
+  print("XDG Desktop Folder Exists.")
+else:
+  print("XDG Desktop Folder does not exist.")
